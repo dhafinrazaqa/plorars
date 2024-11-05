5730c5baf46e46d6bd74ef64a57ea269a32b31a5
Dhafin Razaqa <dhafin.razaqa12@gmail.com>
2024-11-05 19:50:32 +0700

    footer done

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="footer d-flex justify-content-between align-items-center p-navbar py-5 text-white">
+    <img src="{{ asset('images/plorars_footer.png') }}" alt="">
+    <p class="font-poppins fs-6 fw-normal">Copyright © 2024 Plorars</p>
+    <div class="contact-us d-flex flex-column gap-2">
+        <h3 class="font-raleway fs-4 fw-bold text-center">Contact Us</h3>
+        <div class="socials d-flex gap-3">
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=plorarsid@gmail.com&subject=Topik Pesan&body=Teks awal badan pesan"
+                target="_blank">
+                <img src="{{ asset('images/email.png') }}" alt=""></a>
+            <a href="https://wa.me/+6282268882235">
+                <img src="{{ asset('images/whatsapp.png') }}" alt=""></a>
+            <a href="https://www.instagram.com/plorarsid">
+                <img src="{{ asset('images/instagram.png') }}" alt=""> </a>
+        </div>
+    </div>
+</div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary font-roboto fs-7 shadow p-navbar">
     <div class="container-fluid letter-spacing-3">
-        {{-- <a class="navbar-brand" href="#">Plorars</a> --}}
         <a class="navbar-brand me-5" href="#">
             <img src="{{ asset('images/navbar_logo.svg') }}" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav gap-4">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">HOME</a>
@@ -24,9 +23,9 @@
                 </li>
             </ul>
             <button type="button"
-                class="btn btn-primary letter-spacing-3 px-4 py-2 rounded-pill button-gradient fs-7 ms-auto">SIGN
-                IN</button>
+                class="btn btn-primary letter-spacing-3 px-4 py-2 rounded-pill button-gradient fs-7 ms-auto">
+                SIGN IN
+            </button>
         </div>
-
     </div>
 </nav>
