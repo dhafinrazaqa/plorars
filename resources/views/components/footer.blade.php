4112f4ac8e25e7b9a810113c17da9c6ab0cf494a
Dhafin Razaqa <dhafin.razaqa12@gmail.com>
2024-11-05 19:51:21 +0700

    clean up code footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,18 @@
-<div class="footer d-flex justify-content-between align-items-center p-navbar py-5 text-white">
+<footer class="footer d-flex justify-content-between align-items-center p-navbar py-5 text-white">
     <img src="{{ asset('images/plorars_footer.png') }}" alt="">
-    <p class="font-poppins fs-6 fw-normal">Copyright © 2024 Plorars</p>
+    <p class="copyright font-poppins fs-6 fw-normal">Copyright &copy; 2024 Plorars</p>
     <div class="contact-us d-flex flex-column gap-2">
         <h3 class="font-raleway fs-4 fw-bold text-center">Contact Us</h3>
         <div class="socials d-flex gap-3">
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=plorarsid@gmail.com&subject=Topik Pesan&body=Teks awal badan pesan"
-                target="_blank">
-                <img src="{{ asset('images/email.png') }}" alt=""></a>
+            <a href="mailto:plorarsid@gmail.com?subject=Topik Pesan&body=Teks awal badan pesan" target="_blank">
+                <img src="{{ asset('images/email.png') }}" alt="">
+            </a>
             <a href="https://wa.me/+6282268882235">
-                <img src="{{ asset('images/whatsapp.png') }}" alt=""></a>
+                <img src="{{ asset('images/whatsapp.png') }}" alt="">
+            </a>
             <a href="https://www.instagram.com/plorarsid">
-                <img src="{{ asset('images/instagram.png') }}" alt=""> </a>
+                <img src="{{ asset('images/instagram.png') }}" alt="">
+            </a>
         </div>
     </div>
-</div>
+</footer>
