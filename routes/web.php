e7577d031e34bd9b4ad357838a67a34c2c5cbf83
Dhafin Razaqa <dhafin.razaqa12@gmail.com>
2024-11-08 10:55:39 +0700

    ngubah route dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/search', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
